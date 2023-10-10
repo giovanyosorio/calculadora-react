7140c939b23be7634c9c2756727adfbcd2f37cbb
giovany osorio <30839218+giovanyosorio@users.noreply.github.com>
2023-10-10 00:08:03 +0000

    styles main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 
 import logoFreeCodeCamp from "../src/imagenes/freecodecamp-logo.png"
+import './App.css'
 
 function App() {
 
   return (
     <div className='App'>
       <div className='freecodecamp-logo-contenedor'>
-      <img src={logoFreeCodeCamp} className="freecodecapm-logo" alt="logo freecodecamp" />
+      <img src={logoFreeCodeCamp} className="freecodecamp-logo" alt="logo freecodecamp" />
+
+      </div>
+
+      <div className="contenedor-calculadora">
+
 
       </div>
     </div>
+    
   )
 }
 
